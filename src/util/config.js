ed0b97969825770e912bd391d6ebcfc4f76246bb
Thomas Peet <tomaspemora@gmail.com>
2023-07-17 00:54:48 -0400

    fix localhost login

--- src/util/config.js
+++ src/util/config.js
@@ -4,7 +4,7 @@ export const config = {
   studentDomain: "edu_id",
   teacherDomain: "surf_conext",
   extensionsRootUrl: "http://127.0.0.1:8000/static",
-  eduId: "https://mijn.test.eduid.nl",
+  eduId: "http://localhost:4000",
   isDemoEnvironment: 0,
   importBadgeAllowed: !0,
   welcomeBadgeClassName: "Edubadge account complete",
